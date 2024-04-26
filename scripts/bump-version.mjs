34b18d011ff3b072b212c8e7ce86e948ab207281
tzachbon <tzachbonfil@gmail.com>
2024-04-26 17:29:45 +0300

    Remove require from bump version script

--- scripts/bump-version.mjs
+++ scripts/bump-version.mjs
@@ -8,7 +8,8 @@ if (!['patch', 'minor', 'major'].includes(bumpType)) {
 
 execSync(`npm version ${bumpType} --save`, { stdio: 'inherit' });
 
-const { version } = require('../package.json');
+const packageJson = JSON.parse(execSync('cat package.json').toString());
+const version = packageJson.version;
 console.log(`Bumped to version "${version}"`);
 
 execSync(`git tag v${version} -m v${version}`, { stdio: 'inherit' });
